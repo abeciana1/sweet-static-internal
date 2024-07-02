288f8d9a197cc9ae608338b33c45a91b10690ad5
Alex <alex.beciana@gmail.com>
2024-07-01 20:53:49 -0600

    Added daisy ui to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...import("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#0d89ec",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [import("daisyui")],
+};
