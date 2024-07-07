b22ed803a148c9968e08a09f4c886e7732564d41
Alex <alex.beciana@gmail.com>
2024-07-06 22:00:35 -0600

    Imported image component && Rendered logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main>
+      <Image
+        src='https://us-east-1-shared-usea1-02.graphassets.com/cltw9ggr70a3m07lb887p5mfv/cluhe8yedq9pb07k84kwhf0dn'
+        width={500}
+        height={500}
+        alt='Sweet Static logo'
+        className='mx-auto'
+      />
     </main>
   );
 }
